c685d81aef192941ad6c7551483f593ed2eeabba
Benjamin Rowell <brrowell@gmail.com>
2019-12-30 20:56:28 +0000

    Updates to SQL Queries
    Adds NOT NULL to some important columns.
    Adds an UPDATE / UPSERT clause to users, in case of change of level of plan.

--- sql_queries.py
+++ sql_queries.py
@@ -9,53 +9,109 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays (start_time bigint, user_id int, level text, song_id text, artist_id text, session_id text, location text, user_agent text)
+CREATE TABLE IF NOT EXISTS songplays (start_time bigint NOT NULL, 
+                                      user_id int NOT NULL, 
+                                      level text NOT NULL, 
+                                      song_id text, 
+                                      artist_id text, 
+                                      session_id text NOT NULL, 
+                                      location text, 
+                                      user_agent text NOT NULL, 
+                                      PRIMARY KEY (start_time, user_id))
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users (user_id text UNIQUE, first_name text, last_name text, gender text, level text)
+CREATE TABLE IF NOT EXISTS users (user_id text UNIQUE PRIMARY KEY, 
+                                  first_name text NOT NULL, 
+                                  last_name text NOT NULL, 
+                                  gender text NOT NULL, 
+                                  level text NOT NULL)
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs (song_id text UNIQUE, title text, artist_id text, year int, duration int)
+CREATE TABLE IF NOT EXISTS songs (song_id text UNIQUE PRIMARY KEY, 
+                                  title text NOT NULL, 
+                                  artist_id text NOT NULL, 
+                                  year int NOT NULL, 
+                                  duration int NOT NULL)
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists (artist_id text UNIQUE, name text, location text, latitude decimal, longitude decimal)
+CREATE TABLE IF NOT EXISTS artists (artist_id text UNIQUE PRIMARY KEY, 
+                                    name text NOT NULL, 
+                                    location text, 
+                                    latitude decimal, 
+                                    longitude decimal)
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, month int, year int, weekday int)
+CREATE TABLE IF NOT EXISTS time (start_time timestamp PRIMARY KEY, 
+                                 hour int NOT NULL, 
+                                 day int NOT NULL, 
+                                 week int NOT NULL, 
+                                 month int NOT NULL, 
+                                 year int NOT NULL, 
+                                 weekday int NOT NULL)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+INSERT INTO songplays (start_time, 
+                       user_id, 
+                       level, 
+                       song_id, 
+                       artist_id, 
+                       session_id, 
+                       location, 
+                       user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level)
+INSERT INTO users (user_id, 
+                   first_name, 
+                   last_name, 
+                   gender, 
+                   level)
 VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING
+ON CONFLICT (user_id) DO UPDATE
+SET first_name = excluded.first_name,
+    last_name = excluded.last_name,
+    gender = excluded.gender,
+    level = excluded.level
 """)
 
 song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration)
+INSERT INTO songs (song_id, 
+                   title, 
+                   artist_id, 
+                   year, 
+                   duration)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude)
+INSERT INTO artists (artist_id, 
+                     name, 
+                     location, 
+                     latitude, 
+                     longitude)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING
 """)
 
 time_table_insert = ("""
-INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+INSERT INTO time (start_time, 
+                  hour, 
+                  day, 
+                  week, 
+                  month, 
+                  year, 
+                  weekday)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
